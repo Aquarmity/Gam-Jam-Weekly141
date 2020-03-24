10dc506805c39af60737dc6924a671da6d6e5b94
Miguel Diaz <migueldiaz161@gmail.com>
2020-03-24 16:15:40 -0700

    Audio working on Main Menu
    Main Menu has music (temporary version of song and no sfx). I also reorganized some of the scripts into folders, hopefully that doesn't screw anything up.

---
+++ Good Luck Game Jam/Assets/Scripts/MainMenu/MuteButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuteButton : MonoBehaviour
+{
+    bool isMute;
+ 
+    public void Mute ()
+    {
+        isMute = ! isMute;
+        AudioListener.volume =  isMute ? 0 : 1;
+    }
+}
